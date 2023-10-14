4032f27d6e543b37f70426631dc3749b5fbd2fb2
yaromatv <122043732+yaromatv@users.noreply.github.com>
2023-10-14 07:55:43 +0000

    Deploying to gh-pages from @ yaromatv/goit-react-hw-08-phonebook@27e4f9b373a6b1e7480f61df6eef1593bba46dbb 🚀

---
+++ static/js/983.1271d540.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,n){n.r(t);var i=n(3329),c={container:{minHeight:"calc(90vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Welcome to Phonebook"})})}}}]);
+//# sourceMappingURL=983.1271d540.chunk.js.map
