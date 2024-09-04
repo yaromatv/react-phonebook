0ef31bb3c236b3abaad57cdf36a17049fe1f0dcd
Yaroslav Matvieiev <yaromatv@gmail.com>
2024-09-04 17:39:07 +0200

    backend base url update

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import toast from 'react-hot-toast';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
